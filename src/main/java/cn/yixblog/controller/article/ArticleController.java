8857076bbec39885e3a9f8ce56d4e05dee79e894
yixblog <davepotter@163.com>
2014-01-12 12:07:19 +0800

    article api add

--- src/main/java/cn/yixblog/controller/article/ArticleController.java
+++ src/main/java/cn/yixblog/controller/article/ArticleController.java
@@ -15,7 +15,7 @@ import javax.annotation.Resource;
  * Time: 下午5:27
  */
 @RestController
-@RequestMapping("/article")
+@RequestMapping("/art")
 public class ArticleController {
 
     @Resource(name = "articleStorage")
@@ -25,14 +25,24 @@ public class ArticleController {
     private ICommentStorage commentStorage;
 
     @RequestMapping(value = "/articles", method = RequestMethod.GET)
-    public JSONObject listArticlesByJSON(@RequestParam(required = false, value = "keywords[]") String[] keywords, @RequestParam(required = false, defaultValue = "") String startDate,
+    public JSONObject listArticlesByJSON(@RequestParam(required = false) String keywords, @RequestParam(required = false, defaultValue = "") String startDate,
                                          @RequestParam(required = false, defaultValue = "") String endDate, @RequestParam(required = false, defaultValue = "1") int page,
                                          @RequestParam(required = false, defaultValue = "15") int pageSize, @RequestParam(required = false, defaultValue = "addtime") String sortkey,
                                          @RequestParam(required = false, defaultValue = "0") int userId, @RequestParam(required = false) String tag) {
         if ("".equals(tag)) {
             tag = null;
         }
-        return articleStorage.queryArticles(page, pageSize, DateUtils.parseDate(startDate, DateUtils.DATE_FORMAT), DateUtils.parseDate(endDate, DateUtils.DATE_FORMAT), userId, tag, keywords, sortkey);
+        return articleStorage.queryArticles(page, pageSize, DateUtils.parseDate(startDate, DateUtils.DATE_FORMAT), DateUtils.parseDate(endDate, DateUtils.DATE_FORMAT), userId, tag, keywords.split(" "), sortkey);
+    }
+
+    @RequestMapping(value = "/articles/new",method = RequestMethod.GET)
+    public JSONObject listNewArticles(@RequestParam(required = false,defaultValue = "10") int count){
+        return articleStorage.queryArticles(1,count,null,null,0,null,null,"addtime");
+    }
+
+    @RequestMapping(value = "/articles/hot",method = RequestMethod.GET)
+    public JSONObject listHotArticles(@RequestParam(required = false,defaultValue = "10") int count){
+        return articleStorage.queryArticles(1,count,null,null,0,null,null,"replycount");
     }
 
     @RequestMapping(value = "/article/{articleId}", method = RequestMethod.GET)
@@ -46,12 +56,12 @@ public class ArticleController {
     }
 
     @RequestMapping(value = "/hotusers", method = RequestMethod.GET)
-    public JSONObject listHotUsers(@RequestParam(required = false,defaultValue = "10") int topnumber) {
+    public JSONObject listHotUsers(@RequestParam(required = false, defaultValue = "10") int topnumber) {
         return articleStorage.queryArticleAuthors(topnumber);
     }
 
-    @RequestMapping(value = "/article/{articleId}/comments",method = RequestMethod.GET)
-    public JSONObject listArticleComments(@PathVariable int articleId,@RequestParam(defaultValue = "1",required = false) int page,@RequestParam(defaultValue = "20",required = false) int pageSize){
-        return commentStorage.queryArticleComments(articleId,page,pageSize);
+    @RequestMapping(value = "/article/{articleId}/comments", method = RequestMethod.GET)
+    public JSONObject listArticleComments(@PathVariable int articleId, @RequestParam(defaultValue = "1", required = false) int page, @RequestParam(defaultValue = "20", required = false) int pageSize) {
+        return commentStorage.queryArticleComments(articleId, page, pageSize);
     }
 }
